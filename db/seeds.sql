9cd4cdabe6308739d1d2524dd02ee3337419d533
ehui eric <ehui_eric@yahoo.fr>
2021-02-19 22:15:48 -0500

    DB setup

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (id, burger_name , devoured)
+VALUES (1 , "turkey burger" , false);
+
+INSERT INTO burgers (id, burger_name , devoured)
+VALUES (2 , "double burger" , false);
+
+INSERT INTO burgers (id, burger_name , devoured)
+VALUES (3 , "big mac" , false);
