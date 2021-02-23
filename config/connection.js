d279912ae95bcdde04e76689260bbed84ef55442
ehui eric <ehui_eric@yahoo.fr>
2021-02-23 14:59:48 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,26 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
+var connection;
 
-  // Your port; if not 3306
-  port: 3306,
 
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "Assemian-1",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+	  host: "localhost",
+	  user: "root",
+	  password: "Assemian-1",
+	  database: "burgers_db"
+	});
+}
 
 connection.connect(function(err) {
-  if (err) throw err;
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
-module.exports = connection ;
+
+module.exports = connection;
